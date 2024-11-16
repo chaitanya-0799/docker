c19f171e250ae03ff5174997464e54d75ce14bb3
Chaitanya <chai@gmail.com>
2024-11-16 17:08:37 +0530

    adding docker_rmi file

--- docker_images_rmi.sh
+++ docker_images_rmi.sh
@@ -1,15 +1,31 @@
-#! /bin/bash
+#!/bin/bash
 
-docker images >> images
+docker images > delete
 
-num=$(wc -l images | awk '{print $1}')
+# Count the number of lines in the 'delete' file
+
+num=$(wc -l < delete)
+
+# Start from the second line to skip the header
 i=2
 
-while [ i <= $num ]
-do 
-	name=$(sed -n '$i p' images | awk '{print$1}' )
-	echo $name
-	i=$((i+1))
-done << images
+# Loop through all images and remove them
 
-rm images
+while [ $i -le $num ]; do
+    
+    # Get the image name and tag from the 'delete' file
+    
+    name=$(sed -n "${i}p" delete | awk '{print $1}')
+    tag=$(sed -n "${i}p" delete | awk '{print $2}')
+
+    # Remove the Docker image
+    
+    docker rmi "$name:$tag"
+
+    # Increment the line counter
+    
+    i=$((i+1))
+done
+
+# Clean up by removing the temporary 'delete' file
+rm -f delete
